fa76c4bd52208ab42d13fd0b40a993423bfa1737
Jeffrey de Lange <git@jeffreydelange.nl>
2017-07-11 11:36:45 +0200

    Rebuild P class to be less complex

--- drf_composable_permissions/p.py
+++ drf_composable_permissions/p.py
@@ -1,53 +1,62 @@
-import inspect
-
-from rest_framework.permissions import BasePermission
-
-
-class P(BasePermission):
-    OR = 'OR'
-    AND = 'AND'
-    NOT = 'NOT'
-    IDENTITY = 'IDENTITY'
-
-    def _run_operator(self, operator, f1, f2):
-        if operator == self.IDENTITY:
-            return f1()
-        if operator == self.NOT:
-            return not f1()
-        if operator == self.OR:
-            return f1() or f2()
-        if operator == self.AND:
-            return f1() and f2()
-
-        raise NotImplementedError('Operator `{}` is not implemented.'.format(operator))
-
-    def _build_function(self, name):
-        # Encapsulate functions to have lazy evaluation
-        def f(*args, **kwargs):
-            def f1():
-                return getattr(self.c1, name)(*args, **kwargs)
-
-            def f2():
-                return getattr(self.c2, name)(*args, **kwargs)
-            return self._run_operator(self.operator, f1, f2)
-        return f
-
-    def __init__(self, c1, c2=None, operator=IDENTITY):
-        self.c1 = c1() if inspect.isclass(c1) else c1
-        self.c2 = c2() if inspect.isclass(c2) else c2
-        self.operator = operator
-
-        self.has_permission = self._build_function('has_permission')
-        self.has_object_permission = self._build_function('has_object_permission')
-
+class Composable(object):
     def __or__(self, other):
-        return P(self, other, operator=self.OR)
+        return Or(self, other)
 
     def __and__(self, other):
-        return P(self, other, operator=self.AND)
+        return And(self, other)
 
     def __invert__(self):
-        return P(self, operator=self.NOT)
+        return Not(self)
 
     def __call__(self):
         return self
+
+
+class Or(Composable):
+    def __init__(self, c1, c2):
+        self.c1 = c1
+        self.c2 = c2
+
+    def has_permission(self, request, view):
+        return self.c1().has_permission(request, view) or \
+            self.c2().has_permission(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        return self.c1().has_object_permission(request, view, obj) or \
+            self.c2().has_object_permission(request, view, obj)
+
+
+class And(Composable):
+    def __init__(self, c1, c2):
+        self.c1 = c1
+        self.c2 = c2
+
+    def has_permission(self, request, view):
+        return self.c1().has_permission(request, view) and \
+            self.c2().has_permission(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        return self.c1().has_object_permission(request, view, obj) and \
+            self.c2().has_object_permission(request, view, obj)
+
+
+class Not(Composable):
+    def __init__(self, c1):
+        self.c1 = c1
+
+    def has_permission(self, request, view):
+        return not self.c1().has_permission(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        return not self.c1().has_object_permission(request, view, obj)
+
+
+class P(Composable):
+    def __init__(self, c1):
+        self.c1 = c1
+
+    def has_permission(self, request, view):
+        return self.c1().has_permission(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        return self.c1().has_object_permission(request, view, obj)
